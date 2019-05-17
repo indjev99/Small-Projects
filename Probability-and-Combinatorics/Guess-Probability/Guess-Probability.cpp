0e6e048c0a47ea7d33a5e0b1fe939cb709fa95bf
Emil Indzhev <emil.indjev@gmail.com>
2019-05-17 22:38:33 +0300

    Fixed bug - works
    It works!

--- Probability-and-Combinatorics/Guess-Probability/Guess-Probability.cpp
+++ Probability-and-Combinatorics/Guess-Probability/Guess-Probability.cpp
@@ -13,12 +13,11 @@
     is the probability of getting those results from the flips given that q_i = p_i.
     Notice that this metric is maximized when q_i = p_i, therefore B has no incentive to lie.
 
-    We want to know which percentile the result B got is in.
+    We want to know which percentile the result B got is in (the p-value).
     We first try to model the distribution as a normal distribution,
-    and find the number of standard deviations below the mean.
+    and find the number of standard deviations below the mean (Z test).
 
-    Now we want to know the expected value of S - E[S] and find the difference of that with S.
-    Then we need to divide that by the standard deviation of S and finally covert to a p-value.
+    We need to find E[S] and SD[S].
     Some algebraic transformations are needed to improve numerical accuracy.
     Also we will usually work in log-scale.
 
@@ -39,14 +38,16 @@
     E[S^2] = E[s^2]^n
     Var[S] = E[S^2] - E[S]^2
            = E[S^2] * (1 - E[S]^2 / E[S^2])
-    SD[S] = Sqrt(Var[S])
+    SD[S] = sqrt(Var[S])
     Devs  = (S - E[S]) / SD[S]
           = S / SD[S] - E[S] / SD[S]
 
     The problem with this approach is that S has a very skewed distribution.
     It is nowhere near a normal distribution, so the result we get is not useful at all.
 
-    However, we can see that log(S) actually forms a normal distribution, so we can do this there.
+    However, we can see that log(S) approximately forms a normal distribution, so we can do a Z test to it.
+    The larger N is the closer it is to a normal distribution.
+
     E_p[log(s)] = p * log(p) + (1-p) * log(1-p)
     E[log(s)] = integral{0,1}(f_p(x) * (x * log(x) + (1-x) * log(1-x)) * dx)
     S = s_1 * s_2 * ... * s_n
@@ -56,8 +57,15 @@
     log(S)^2 = (log(s_1) + log(s_2) + ... + log(s_n))^2
     E[log(S)^2] = E[log(s)^2] * n + E[log(s_1)*log(s_2)] * n * (n-1)
                 = E[log(s)^2] * n + E[log(s)]^2 * n * (n-1)
+    Var[log(S)] = E[log(S)^2] - E[log(S)]^2
+    SD[log(S)] = sqrt(Var[log(S)])
 
-    Final. */
+    Z = (log(S) - E[log(S)]) / SD[log(S)]
+    p = 2 * Phi(-|Z|)
+
+    Assuming the p.d.f. that we have is correct:
+    If Z < 0, a low p-value means that B does not know the probabilities.
+    If Z > 0, a low p-value means that B can predict the future. */
 
 #include <iostream>
 #include <algorithm>
@@ -91,29 +99,72 @@ public:
     }
 };
 
+struct CoinGeneratorTriangular : CoinGenerator
+{
+    std::uniform_real_distribution<double> uniform;
+public:
+    CoinGeneratorTriangular()
+    {
+        uniform=std::uniform_real_distribution<double>(0,1);
+    }
+    double pdf(double x) const
+    {
+        return 2*x;
+    }
+    double sample()
+    {
+        return sqrt(uniform(generator));
+    }
+};
+
+struct CoinGeneratorFairish : CoinGenerator
+{
+    std::uniform_real_distribution<double> uniform;
+    double range;
+public:
+    CoinGeneratorFairish(double range): range(range)
+    {
+        uniform=std::uniform_real_distribution<double>(0.5-range/2,0.5+range/2);
+    }
+    double pdf(double x) const
+    {
+        if (x<0.5-range/2 || x>0.5+range/2) return 0;
+        return 1/range;
+    }
+    double sample()
+    {
+        return uniform(generator);
+    }
+};
+
 struct Predictor
 {
 public:
-    virtual double predict(double p) const =0;
+    virtual double predict(CoinGenerator* coinGenerator, double p) =0;
 };
 
 struct PredictorCorrect : Predictor
 {
 public:
-    double predict(double p) const
+    double predict(CoinGenerator* coinGenerator, double p)
     {
         return p;
     }
 };
 
-struct PredictorBiased : Predictor
+struct PredictorSometimes : Predictor
 {
-    const double bias;
+    std::uniform_real_distribution<double> uniform;
+    const double knowledge;
 public:
-    PredictorBiased(double bias): bias(bias) {}
-    double predict(double p) const
+    PredictorSometimes(double knowledge): knowledge(knowledge)
     {
-        return p*bias;
+        uniform=std::uniform_real_distribution<double>(0,1);
+    }
+    double predict(CoinGenerator* coinGenerator, double p)
+    {
+        if (uniform(generator)<knowledge) return p;
+        return coinGenerator->sample();
     }
 };
 
@@ -140,13 +191,13 @@ double safe_sum(std::vector<double>& v)
     return total;
 }
 
-double find_ln_score(CoinGenerator* coinGenerator, const Predictor* predictor, int n)
+double find_ln_score(CoinGenerator* coinGenerator, Predictor* predictor, int n)
 {
     std::vector<double> elems;
     for (int i=0;i<n;++i)
     {
         double p=coinGenerator->sample();
-        double q=predictor->predict(p);
+        double q=predictor->predict(coinGenerator,p);
         int f=flip(p);
         /*if (f) q=0.9;
         else q=0.1;*/
@@ -216,9 +267,9 @@ double find_expected_ln_score_squared(const CoinGenerator* coinGenerator, int n,
     return elns2*n + elns*elns*n*(n-1);
 }
 
-const int N=50;
+const int N=1e5;
 const int STEPS=1e6;
-const int TRIALS=1e5;
+const int TRIALS=10;
 
 #include <conio.h>
 #include <fstream>
@@ -227,9 +278,14 @@ int main()
     // Initialize objects
 
     CoinGenerator* coinGeneratorUniform=new CoinGeneratorUniform();
+    CoinGenerator* coinGeneratorTriangular=new CoinGeneratorTriangular();
+    CoinGenerator* coinGeneratorFairish=new CoinGeneratorFairish(0.5);
     Predictor* predictorCorrect=new PredictorCorrect();
-    Predictor* predictorBiased=new PredictorBiased(1);
+    Predictor* predictorSometimes=new PredictorSometimes(0.99);
 
+
+    CoinGenerator* coinGenerator=coinGeneratorFairish;
+    Predictor* predictor=predictorSometimes;
 
     // Find theoretic results for S
 
@@ -241,18 +297,17 @@ int main()
     double lnEoverSD;
     double lnSoverSD;
 
-    lnExpScore=find_ln_expected_score(coinGeneratorUniform,N,STEPS);
-    lnExpScore2=find_ln_expected_score_squared(coinGeneratorUniform,N,STEPS);
+    lnExpScore=find_ln_expected_score(coinGenerator,N,STEPS);
+    lnExpScore2=find_ln_expected_score_squared(coinGenerator,N,STEPS);
     lnExp2Score=lnExpScore*2;
     lnVarScore=lnExpScore2+log(1-exp(lnExp2Score-lnExpScore2));
     lnSDScore=lnVarScore/2;
     lnEoverSD=lnExpScore-lnSDScore;
 
-    std::cout<<"E[S]: "<<lnExpScore<<' '<<exp(lnExpScore)<<std::endl;
+    /*std::cout<<"E[S]: "<<lnExpScore<<' '<<exp(lnExpScore)<<std::endl;
     std::cout<<"Var[S]: "<<lnVarScore<<' '<<exp(lnVarScore)<<std::endl;
     std::cout<<"SD[S]: "<<lnSDScore<<' '<<exp(lnSDScore)<<std::endl;
-    std::cout<<"E[S] / SD[S]: "<<lnEoverSD<<' '<<exp(lnEoverSD)<<std::endl;
-    std::cout<<std::endl;
+    std::cout<<std::endl;*/
 
 
     // Find theoretic results for log(S)
@@ -263,15 +318,13 @@ int main()
     double varLnScore;
     double SDLnScore;
 
-    expLnScore=find_expected_ln_score(coinGeneratorUniform,N,STEPS);
-    expLnScore2=find_expected_ln_score_squared(coinGeneratorUniform,N,STEPS);
+    expLnScore=find_expected_ln_score(coinGenerator,N,STEPS);
+    expLnScore2=find_expected_ln_score_squared(coinGenerator,N,STEPS);
     exp2LnScore=expLnScore*expLnScore;
     varLnScore=expLnScore2-exp2LnScore;
     SDLnScore=sqrt(varLnScore);
 
     std::cout<<"E[log(S)]: "<<expLnScore<<std::endl;
-    std::cout<<"E[log(S)^2]: "<<expLnScore2<<std::endl;
-    std::cout<<"E[log(S)]^2: "<<exp2LnScore<<std::endl;
     std::cout<<"Var[log(S)]: "<<varLnScore<<std::endl;
     std::cout<<"SD[log(S)]: "<<SDLnScore<<std::endl;
     std::cout<<std::endl;
@@ -296,17 +349,20 @@ int main()
 
     for (int i=0;i<TRIALS;++i)
     {
+        lnScore=find_ln_score(coinGenerator,predictor,N);
+        lnSoverSD=lnScore-lnSDScore;
+
         dataOut<<lnScore<<'\t'<<exp(lnScore)<<'\n';
 
-        lnScore=find_ln_score(coinGeneratorUniform,predictorBiased,N);
-        lnSoverSD=lnScore-lnSDScore;
-        double devs=exp(lnSoverSD)-exp(lnEoverSD);
-
-        /*std::cout<<"S: "<<lnScore<<' '<<exp(lnScore)<<std::endl;
-        std::cout<<"S / SD[S]: "<<lnSoverSD<<' '<<exp(lnSoverSD)<<std::endl;
+        /*std::cout<<"S: "<<exp(lnScore)<<std::endl;
         std::cout<<"S - E[S]: "<<exp(lnScore)-exp(lnExpScore)<<std::endl;
-        std::cout<<"(S - E[S]) / SD[S]: "<<devs<<std::endl;
+        std::cout<<"Z: "<<exp(lnSoverSD)-exp(lnEoverSD)<<std::endl;
         std::cout<<std::endl;*/
+
+        std::cout<<"log(S): "<<lnScore<<std::endl;
+        std::cout<<"log(S) - E[log(S)]: "<<lnScore-expLnScore<<std::endl;
+        std::cout<<"Z log: "<<(lnScore-expLnScore)/SDLnScore<<std::endl;
+        std::cout<<std::endl;
 
         expSVect.push_back(exp(lnScore));
 
@@ -331,10 +387,10 @@ int main()
 
     empVarLnScore=safe_sum(varLnSVect)/TRIALS;
 
-    std::cout<<"Empiric E[S]: "<<empExpScore<<std::endl;
+    /*std::cout<<"Empiric E[S]: "<<empExpScore<<std::endl;
     std::cout<<"Empiric Var[S]: "<<empVarScore<<std::endl;
     std::cout<<"Empiric SD[S]: "<<sqrt(empVarScore)<<std::endl;
-    std::cout<<std::endl;
+    std::cout<<std::endl;*/
 
     std::cout<<"Empiric E[log(S)]: "<<empExpLnScore<<std::endl;
     std::cout<<"Empiric Var[log(S)]: "<<empVarLnScore<<std::endl;
