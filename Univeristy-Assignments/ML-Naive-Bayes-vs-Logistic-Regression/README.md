b6ccd4d97fc1a58767d4728107ee314a4af330bc
Emil Indzhev <emil.indjev@gmail.com>
2019-11-22 01:08:55 +0200

    Extensions and other datasets
    Naive Bayes now properly handles missing values. Added another dataset.
    
    TO-DO: Experiment how to handle missing values with logistic regression.

---
+++ Univeristy-Assignments/ML-Naive-Bayes-vs-Logistic-Regression/README.md
@@ -0,0 +1,5 @@
+My implementation of a Naive Bayes classifier tested against a library implementation of logistic regression on several datasets.
+
+Data source: https://archive.ics.uci.edu/ml/index.php
+
+The file conversion script is an extension of one provided by the university.
---
+++ Univeristy-Assignments/ML-Naive-Bayes-vs-Logistic-Regression/convert_adult.py
@@ -0,0 +1,78 @@
+from numpy import loadtxt
+
+import argparse
+import _pickle as cp
+import numpy as np
+
+
+def load_raw_data(filename):
+    raw_data = loadtxt(filename, delimiter=',', skiprows=0, dtype=str)
+    return raw_data
+
+
+def process_data(raw_data, feature_types, include_missing=False):
+    N_data = raw_data.shape[0]
+    N_features = raw_data.shape[1] - 1
+
+    X = np.zeros([N_data, N_features])
+    y = np.zeros(N_data)
+
+    complete = np.ones(N_data, dtype=bool)
+
+    # Find possible values of categorical features
+    feature_dicts = list()
+    for i in range(0, N_features + 1):
+        if i == N_features or feature_types[i] != 'r':
+            unique = np.unique(raw_data[:, i])
+            feature_dicts.append(unique[unique != ' ?'])
+        else:
+            feature_dicts.append(np.array([]))
+    feature_dicts = np.array(feature_dicts)
+
+    # Add data as np arrays
+    for i in range(N_data):
+        y[i] = np.where(feature_dicts[-1] == raw_data[i, -1])[0][0]
+        for j in range(0, N_features):
+            if raw_data[i, j] == ' ?':
+                X[i, j] = -1.
+                complete[i] = False
+            elif feature_types[j] == 'r':
+                X[i, j] = float(raw_data[i, j])
+            else:
+                X[i, j] = np.where(feature_dicts[j] == raw_data[i, j])[0][0]
+
+    # Shuffle data before storing
+    np.random.seed(0)
+    shuffle = np.random.permutation(N_data)
+    X = X[shuffle]
+    y = y[shuffle]
+    complete = complete[shuffle]
+
+    if include_missing is True:
+        return (X, y)
+    else:
+        return (X[complete], y[complete])
+
+name = 'adult'
+feature_types = 'rcrcrcccccrrrc'
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--include-missing', dest='include_missing',
+                        action='store_true')
+    parser.set_defaults(include_missing=False)
+    args = parser.parse_args()
+
+    if args.include_missing is True:
+        filename = name + '_full'
+    else:
+        filename = name
+
+    input_filename = name + '.data'
+
+    raw_data = load_raw_data(input_filename)
+
+    adult = process_data(raw_data, feature_types, include_missing=args.include_missing)
+
+    ff_file = open(filename + '.pickle', 'wb')
+    cp.dump(adult, ff_file)
