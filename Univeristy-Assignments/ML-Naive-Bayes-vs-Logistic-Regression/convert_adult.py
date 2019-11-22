4e7a3bcc5f27daf0ee4b0dac8e46142c1bffc991
Emil Indzhev <emil.indjev@gmail.com>
2019-11-22 13:33:33 +0200

    One hot encoding, bernoulli, ugly dataset for demo

--- Univeristy-Assignments/ML-Naive-Bayes-vs-Logistic-Regression/convert_adult.py
+++ Univeristy-Assignments/ML-Naive-Bayes-vs-Logistic-Regression/convert_adult.py
@@ -10,14 +10,9 @@ def load_raw_data(filename):
     return raw_data
 
 
-def process_data(raw_data, feature_types, include_missing=False):
+def process_data(raw_data, feature_types, ugly=False, include_missing=False):
     N_data = raw_data.shape[0]
     N_features = raw_data.shape[1] - 1
-
-    X = np.zeros([N_data, N_features])
-    y = np.zeros(N_data)
-
-    complete = np.ones(N_data, dtype=bool)
 
     # Find possible values of categorical features
     feature_dicts = list()
@@ -29,17 +24,30 @@ def process_data(raw_data, feature_types, include_missing=False):
             feature_dicts.append(np.array([]))
     feature_dicts = np.array(feature_dicts)
 
+    complete = np.ones(N_data, dtype=bool)
+
     # Add data as np arrays
-    for i in range(N_data):
-        y[i] = np.where(feature_dicts[-1] == raw_data[i, -1])[0][0]
-        for j in range(0, N_features):
-            if raw_data[i, j] == ' ?':
-                X[i, j] = -1.
-                complete[i] = False
-            elif feature_types[j] == 'r':
-                X[i, j] = float(raw_data[i, j])
-            else:
-                X[i, j] = np.where(feature_dicts[j] == raw_data[i, j])[0][0]
+    if ugly:
+        X = np.full([N_data, N_features], 'ERRORERRORERRORERRORERROR')
+        y = np.full(N_data, 'ERRORERRORERRORERRORERROR')
+        for i in range(N_data):
+            y[i] = raw_data[i][-1][1:]
+            X[i, 0] = raw_data[i][0]
+            for j in range(1, N_features):
+                X[i, j] = raw_data[i][j][1:]
+    else:
+        X = np.zeros([N_data, N_features])
+        y = np.zeros(N_data)
+        for i in range(N_data):
+            y[i] = np.where(feature_dicts[-1] == raw_data[i, -1])[0][0]
+            for j in range(0, N_features):
+                if raw_data[i, j] == ' ?':
+                    X[i, j] = -1.
+                    complete[i] = False
+                elif feature_types[j] == 'r':
+                    X[i, j] = float(raw_data[i, j])
+                else:
+                    X[i, j] = np.where(feature_dicts[j] == raw_data[i, j])[0][0]
 
     # Shuffle data before storing
     np.random.seed(0)
@@ -48,7 +56,7 @@ def process_data(raw_data, feature_types, include_missing=False):
     y = y[shuffle]
     complete = complete[shuffle]
 
-    if include_missing is True:
+    if (ugly or include_missing) is True:
         return (X, y)
     else:
         return (X[complete], y[complete])
@@ -58,12 +66,17 @@ feature_types = 'rcrcrcccccrrrc'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--ugly', dest='ugly',
+                        action='store_true')
+    parser.set_defaults(ugly=False)
     parser.add_argument('--include-missing', dest='include_missing',
                         action='store_true')
     parser.set_defaults(include_missing=False)
     args = parser.parse_args()
 
-    if args.include_missing is True:
+    if args.ugly is True:
+        filename = name + '_ugly'
+    elif args.include_missing is True:
         filename = name + '_full'
     else:
         filename = name
@@ -72,7 +85,7 @@ if __name__ == "__main__":
 
     raw_data = load_raw_data(input_filename)
 
-    adult = process_data(raw_data, feature_types, include_missing=args.include_missing)
+    adult = process_data(raw_data, feature_types, ugly=args.ugly, include_missing=args.include_missing)
 
     ff_file = open(filename + '.pickle', 'wb')
     cp.dump(adult, ff_file)
